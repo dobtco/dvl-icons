9bb4b7f37fc5faca8519c305501777e582964c5f
Adam Becker <ad@mbecker.cc>
2015-10-29 12:24:04 -0700

    new file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,37 @@ module.exports = function(grunt){
     webfont: {
       icons: {
         src: 'icons/*.svg',
-        dest: 'dist',
+        dest: 'tmp',
         options: {
           font: 'dvl-icons',
           hashes: false,
           stylesheet: 'scss',
+          template: 'support/template.css',
           htmlDemoTemplate: 'support/demo_template.html',
           codepointsFile: 'support/codepoints.json'
         }
       }
+    },
+    copy: {
+      fonts: {
+        cwd: 'tmp/',
+        src: ['*.{eot,ttf,woff}'],
+        dest: 'app/assets/fonts',
+        expand: true
+      },
+      stylesheets: {
+        src: 'tmp/_dvl-icons.scss',
+        dest: 'app/assets/stylesheets/dvl-icons.scss'
+      }
+    },
+    clean: {
+      compiled: ['tmp']
     }
   });
 
   grunt.loadNpmTasks('grunt-webfont');
-  grunt.registerTask('default', ['webfont']);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('default', ['webfont', 'copy', 'clean']);
 }
