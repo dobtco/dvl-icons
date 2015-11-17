6686ef36e75dc12c707ed7b0f21f90d89613ce2c
Josh Rubenoff <josh@dobt.co>
2015-11-16 22:14:29 -0800

    Set font-height to 768
    24 (base size) x 32

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt){
           font: 'dvl-icons',
           hashes: false,
           stylesheet: 'scss',
-          fontHeight: 24,
+          fontHeight: 768,
           template: 'support/template.css',
           htmlDemoTemplate: 'support/demo_template.html',
           codepointsFile: 'support/codepoints.json'
